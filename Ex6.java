31e210864340796c6b03f028b4d10c2cda47d302
Erick Alexsandro <111057605+erick-alexsandro@users.noreply.github.com>
2024-03-24 13:38:44 -0300

    Finished exercise 6

---
+++ Ex6.java
@@ -0,0 +1,15 @@
+
+public class Ex6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("---------------------------------------");
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
